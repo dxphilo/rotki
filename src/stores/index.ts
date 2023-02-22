fd64261f7979f9789e5c56925f0cb24f6cce6309
ozark0 <developerphilo@gmail.com>
2023-02-22 23:47:15 +0300

    refactors: store refactors to use composition-api

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,40 +1,37 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 import axios from "axios";
 
 import { baseUrl } from "@/env";
 import { errorHandler } from "@/helpers/index";
-import type { rootStore } from "@/types/index";
+import type { AccountBalances, AccountEvents } from "@/types/index";
 
-export const useRootStore = defineStore("rootStore", {
-  state: (): rootStore => {
-    return {
-      userBalances: {},
-      userEvents: {},
-    };
-  },
-  actions: {
-    async fetchBalances(): Promise<void> {
-      try {
-        const response = await axios.get(`${baseUrl}/balances`);
-        const data = await response.data.result;
-        this.userBalances = data;
-      } catch (error: unknown) {
-        // handle possible errors
-        errorHandler(error);
-      }
-    },
-    async fetchEvents(): Promise<void> {
-      try {
-        const response = await axios.get(`${baseUrl}/events`, {
-          responseType: "json",
-        });
-        const data = await response.data.result;
-        this.userEvents = data;
-      } catch (error: unknown) {
-        // handle possible errors
-        errorHandler(error);
-      }
-    },
-  },
-  getters: {},
+export const useRootStore = defineStore("rootStore", () => {
+  const userBalances = ref<Record<string, AccountBalances>>({});
+  const userEvents = ref<Record<string, AccountEvents>>({});
+
+  async function fetchBalances(): Promise<void> {
+    try {
+      const response = await axios.get(`${baseUrl}/balances`);
+      const data = await response.data.result;
+      userBalances.value = data;
+    } catch (error: unknown) {
+      // handle possible errors
+      errorHandler(error);
+    }
+  }
+
+  async function fetchEvents(): Promise<void> {
+    try {
+      const response = await axios.get(`${baseUrl}/events`, {
+        responseType: "json",
+      });
+      const data = await response.data.result;
+      userEvents.value = data;
+    } catch (error: unknown) {
+      // handle possible errors
+      errorHandler(error);
+    }
+  }
+  return { userBalances, userEvents, fetchEvents, fetchBalances };
 });
